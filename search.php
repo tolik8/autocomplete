a6d3e52e7d4bb47c505adb4de19d2319f71c30e5
tolik8 <tolik8@gmail.com>
2018-10-17 13:54:03 +0300

    change database connect to PDO

--- search.php
+++ search.php
@@ -2,27 +2,39 @@
 
 // Database configuration
 $dbHost     = 'localhost';
+$dbName     = 'test';
 $dbUsername = 'test';
 $dbPassword = '';
-$dbName     = 'test';
+$dbCharset  = 'utf8';
+$dbDSN      = "mysql:host=$dbHost;dbname=$dbName;charset=$dbCharset";
+
+$dbOptions = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 // Connect with the database
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+$db = new PDO($dbDSN, $dbUsername, $dbPassword, $dbOptions);
 
 // Get search term
 $searchTerm = $_GET['term'];
+if (trim($searchTerm) == '') {exit;}
+$searchTerm = "%" . $searchTerm . "%";
 
 // Get matched data from skills table
-$query = $db->query("SELECT * FROM skills WHERE skill LIKE '%".$searchTerm."%' AND status = '1' ORDER BY skill ASC");
+$sql = "SELECT id, skill FROM skills WHERE lower(skill) LIKE lower(:search) AND status = '1' ORDER BY skill ASC";
+$stmt = $db->prepare($sql);
+$stmt->bindParam(':search', $searchTerm);
+$stmt->execute();
+$rows = $stmt->fetchAll();
 
 // Generate skills data array
-$skillData = array();
-if($query->num_rows > 0){
-    while($row = $query->fetch_assoc()){
-        $data['id'] = $row['id'];
-        $data['value'] = $row['skill'];
-        array_push($skillData, $data);
-    }
+$skillData = [];
+foreach ($rows as $row) {
+    $data['id'] = $row['id'];
+    $data['value'] = $row['skill'];
+    array_push($skillData, $data);
 }
 
 // Return results as json encoded array
