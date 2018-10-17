e36d5093bdebc5f05a0b14e7811fbb677e4c7b91
tolik8 <tolik8@gmail.com>
2018-10-17 18:34:10 +0300

    pdo

---
+++ pdo.php
@@ -0,0 +1,18 @@
+<?php
+
+// Database configuration
+$dbHost     = 'localhost';
+$dbName     = 'test';
+$dbUsername = 'test';
+$dbPassword = '';
+$dbCharset  = 'utf8';
+$dbDSN      = 'mysql:host='.$dbHost.';dbname='.$dbName.';charset='.$dbCharset;
+
+$dbOptions = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+// Connect with the database
+$db = new PDO($dbDSN, $dbUsername, $dbPassword, $dbOptions);
